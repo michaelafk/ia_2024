83792f280268b09f3480ff5674de78660907949c
michaelafk <onedeath24@gmail.com>
2024-10-06 17:03:59 +0200

    Clase estat monedes

---
+++ monedes/Estat.py
@@ -0,0 +1,107 @@
+import copy
+from quiques.joc import Joc
+
+class Estat:
+    def __init__(self,orden_monedes: str,cost: int,euristica: int,pare = None):
+        if pare is None:
+            self.cost = 0
+        self.orden_monedes = orden_monedes
+        self.cost = cost 
+        self.euristica = calculate_euristic(self)
+        self.pare = pare
+   
+def generar_fillsv2(self) -> list:
+    index = self.orden_monedes.index(" ")
+    succesors = []
+    #PRIMERA ACCIO
+    if index == 0:
+        nou_estat = copy.deepcopy(self)
+        nou_estat.orden_monedes = swap_characters(nou_estat.orden_monedes,index,index+1)
+        nou_estat.pare = self
+        nou_estat.cost +=1
+        succesors.append(nou_estat)
+    elif index == len(self.orden_monedes)-1:
+        nou_estat = copy.deepcopy(self)
+        nou_estat.orden_monedes = swap_characters(nou_estat.orden_monedes,index,index-1)
+        nou_estat.pare = self
+        nou_estat.cost +=1
+        succesors.append(nou_estat)
+    else:
+        nou_estat = copy.deepcopy(self)
+        nou_estat.orden_monedes = swap_characters(nou_estat.orden_monedes,index,index+1)
+        nou_estat.pare = self
+        nou_estat.cost +=1
+        succesors.append(nou_estat)
+        nou_estat1 = copy.deepcopy(self)
+        nou_estat1.orden_monedes = swap_characters(nou_estat1.orden_monedes,index,index-1)
+        nou_estat1.pare = self
+        nou_estat.cost +=1
+        succesors.append(nou_estat1)
+    #SEGONA ACCIO
+    for i in range(len(self.orden_monedes)):
+        nou_estat = copy.deepcopy(self)
+        aux = list(nou_estat.orden_monedes)
+        if aux[i].__eq__("C"):
+            aux[i] = "X"
+        elif aux[i].__eq__("X"):
+            aux[i] = "C"
+        aux2 = ''.join(aux)
+        if not aux2.__eq__(self.orden_monedes):
+            nou_estat.orden_monedes = aux2
+            nou_estat.pare = self
+            nou_estat.cost +=2
+            succesors.append(nou_estat)
+    #TERCERA ACCIO
+    for i in range(len(self.orden_monedes)):
+        nou_estat = copy.deepcopy(self)
+        aux = list(nou_estat.orden_monedes)
+        if not aux[i].__eq__(" "):
+            if aux[i].__eq__("C"):
+                aux[i] = "X"
+            else:
+                aux[i] = "C"
+            if i+2 < len(self.orden_monedes) - 1:
+                aux2 = ''.join(aux)
+                aux3 = swap_characters(aux2,i,i+2)
+            else:
+                aux2 = ''.join(aux)
+                aux3 = swap_characters(aux2,i,i-2)
+                nou_estat.orden_monedes = aux3
+            nou_estat.pare = self
+            nou_estat.cost +=2
+            succesors.append(nou_estat)             
+    return succesors
+
+def swap_characters(s, i, j):
+    if i != j:
+    # Convert the string to a list to allow modifications
+        s_list = list(s)
+        # Swap the characters
+        s_list[i], s_list[j] = s_list[j], s_list[i]
+        # Convert the list back to a string
+        s = ''.join(s_list)
+    return s
+
+def get_euristica(self):
+    return self.euristica
+
+def get_cost(self):
+    return self.cost
+
+def es_meta(self):
+    return (self.orden_monedes.__eq__(" XXXC"))
+
+def calculate_euristic(self):
+    orden_monedes_final = " XXXC"
+    lista1 = list(orden_monedes_final)
+    lista2 = list(self.orden_monedes)
+    cont = 0
+    for i in range(len(self.orden_monedes)):
+        if i > 0:
+            if lista1[i]!=lista2[i]:
+                cont+=1 
+        else:
+            index1 = lista1.index(" ")
+            index2 = lista2.index(" ")
+            cont+= abs(index2-index1)
+    return cont
--- monedes/agent.py
+++ monedes/agent.py
@@ -5,9 +5,9 @@ Percepcions:
 Solució:
     " XXXC"
 """
-
+from queue import PriorityQueue
 from base import agent, entorn
-from monedes.joc import AccionsMoneda
+from monedes.joc import AccionsMoneda,Estat
 
 SOLUCIO = " XXXC"
 
@@ -22,9 +22,46 @@ class AgentMoneda(agent.Agent):
     def pinta(self, display):
         print(self._posicio_pintar)
 
+    def cerca(self, estat_inicial: Estat) -> bool:
+        self.__oberts = PriorityQueue()
+        self.__tancats = set()
+        exit = False
+
+        self.__oberts.put((estat_inicial.get_euristica() + estat_inicial.get_cost()),estat_inicial)
+        while self.__oberts:
+            estat_actual = self.__oberts.get()[1]
+
+            if estat_actual in self.__tancats:
+                continue
+
+            if estat_actual.es_meta():
+                break
+
+            for f in estat_actual.genera_fill():
+                self.__oberts.put((f.get_euristica() + f.get_cost()),f)
+
+            self.__tancats.add(estat_actual)
+
+        if estat_actual.es_meta():
+            self.__accions = self.__tancats
+            exit = True
+
+        return exit
     def actua(
         self, percepcio: entorn.Percepcio
-    ) -> entorn.Accio | tuple[entorn.Accio, object]:
+    ) -> entorn.Accio | tuple[entorn.Accio, object]:        
+        if self.__accions is None:
+            estat_inicial = Estat(
+                orden_monedes = "CX CX"
+            )
+            self.cerca(estat_inicial)
+
+        if self.__tancats:
+            estat = self.__cami_exit.pop(0)
+
+            return AccionsMoneda.BOTAR, (quiques, llops)
+        else:
+            return AccionsBarca.ATURAR
         return AccionsMoneda.RES
 
 
